683c16b178265cb904f5e82a1407136a8d3dc116
kiliman <kiliman@gmail.com>
2022-10-12 19:09:35 -0400

    Update seed file

--- avoqado/prisma/seed.js
+++ avoqado/prisma/seed.js
@@ -2,33 +2,84 @@ const { PrismaClient } = require("@prisma/client");
 const db = new PrismaClient();
 
 async function seed() {
-  // await db.restaurant.create({
-  //   data: {
-  //     image:
-  //       "https://madre-cafe.com/wp-content/uploads/2021/11/logo-madre-cafe-header.svg",
-  //     name: "Madre Cafe",
-  //     address: "Calle Orizaba 131",
-  //     zipCode: "06700",
-  //     extraAddress: "Colonia Roma Cdmx",
-  //     rating: 4.8,
-  //     ratingCount: 400,
-  //     cuisine: "Internacional",
-  //   },
-  // });
-  // await db.menu.create({
-  //   data: {
-  //     menuType: "desayuno",
-  //   },
-  // });
-  // await Promise.all(
-  //   getCategories().map((c) => {
-  //     return db.categories.create({ data: c });
-  //   })
-  // );
-  await Promise.all(
-    getDishes().map((d) => {
-      return db.dish.create({ data: d });
-    })
+  const user = await db.user.create({
+    data: {
+      email: "kiliman@gmail",
+      total: 0,
+    },
+  });
+
+  const restaurant = await db.restaurant.create({
+    data: {
+      name: "Madre Cafe",
+      logo: "https://madre-cafe.com/wp-content/uploads/2021/11/logo-madre-cafe-header.svg",
+      email: "info@madrecafe.com",
+      phone: "8885551212",
+    },
+  });
+
+  const branch = await db.branch.create({
+    data: {
+      name: "Madre Cafe #1",
+      ppt_image:
+        "https://madre-cafe.com/wp-content/uploads/2021/11/logo-madre-cafe-header.svg",
+      email: "branch1@madrecafe.com",
+      phone: "8885551212",
+      city: "Guadalajara",
+      address: "Calle Orizaba 131",
+      extraAddress: "Colonia Roma Cdmx",
+      rating: 4.8,
+      rating_quantity: 400,
+      cuisine: "Internacional",
+      restaurant: { connect: { id: restaurant.id } },
+    },
+  });
+
+  for (let i = 1; i <= 10; i++) {
+    const table = await db.table.create({
+      data: {
+        table_number: i,
+        order_in_progress: false,
+        branch: { connect: { id: branch.id } },
+      },
+    });
+  }
+
+  const menu = await db.menu.create({
+    data: {
+      name: "Main Menu",
+      branch: { connect: { id: branch.id } },
+    },
+  });
+
+  const categories = await Promise.all(
+    getCategories().map(({ name }) =>
+      db.menuCategory.create({
+        data: {
+          name,
+          menu: { connect: { id: menu.id } },
+        },
+      })
+    )
+  );
+
+  const menuItems = await Promise.all(
+    categories.flatMap((category, i) =>
+      range(1, 5).map((j) =>
+        db.menuItem.create({
+          data: {
+            name: `${category.name} Item #${j}`,
+            image:
+              "https://madre-cafe.com/wp-content/uploads/2021/11/logo-madre-cafe-header.svg",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: getRandom(5, 20),
+            available: true,
+            menuCategory: { connect: { id: category.id } },
+          },
+        })
+      )
+    )
   );
 }
 
@@ -159,4 +210,15 @@ function getDishes() {
       },
     },
   ];
+}
+
+function range(start, end, step = 1) {
+  return Array.from(
+    { length: (end - start + 1) / step },
+    (_, i) => start + i * step
+  );
+}
+
+function getRandom(start, end) {
+  return Math.floor(Math.random() * (end - start)) + start;
 }
